d43f9407f73fd95497a31b37059286b830e2eabe
administrator-prog <administrator@realtyseo.co>
2026-07-02 14:04:59 +0100

    Fix OAuth logging

--- src/index.ts
+++ src/index.ts
@@ -226,7 +226,9 @@ app.get('/', (_req, res) => {
 
 app.get('/auth/google', (_req, res) => {
   const client = oauthClient();
-  console.log('REDIRECT URI:', GOOGLE_REDIRECT_URI);\n  console.log('CLIENT ID:', GOOGLE_CLIENT_ID);\n  const url = client.generateAuthUrl({
+  console.log('REDIRECT URI:', GOOGLE_REDIRECT_URI);
+  console.log('CLIENT ID:', GOOGLE_CLIENT_ID);
+  const url = client.generateAuthUrl({
     access_type: 'offline',
     prompt: 'consent',
     scope: [
